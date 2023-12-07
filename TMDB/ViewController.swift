4d9e3e8f5986e27d1be2a779e029c22f0c139284
Günter Hertz <gunter.mhertz@gmail.com>
2023-12-07 17:53:13 -0300

    Create a empty project

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Günter Hertz on 07/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
